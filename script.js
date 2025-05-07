561b3c6984bb477f58df4b82e03b787b9078b610
Bharath S J <166963664+Bharath-S-J@users.noreply.github.com>
2025-05-07 20:27:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+let arr = [1, 2, 3, 4];
+let divv = document.getElementById("output");
+
+function filternum() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(arr.filter(n => n % 2 === 0));
+    }, 1000);
+  });
+}
+
+function mulnum(arr) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(arr.map(n => n * 2));
+    }, 3000);
+  });
+}
+
+filternum()
+  .then((data) => {
+    divv.innerText = data;
+    return mulnum(data);
+  })
+  .then((data) => {
+    divv.innerText = data`;
+  });
